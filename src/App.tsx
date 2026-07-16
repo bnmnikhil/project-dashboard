b7197b57714711bf1c415f1d3c1718fa011eff67
Nikhil <b.nikhilbabu1@gmail.com>
2026-07-16 20:56:04 +0530

    fix: switch to HashRouter to fix Cloudflare loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import ProjectDetails from './pages/ProjectDetails';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen bg-background">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/project/:id" element={<ProjectDetails />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
